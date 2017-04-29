d960aa3d647a8b39f0ac918d4827a824610215a0
Michael Twohey <mtwohey2@yahoo.com>
2017-04-29 10:51:48 -0700

    Add project files.

---
+++ Nemsis34/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Nemsis34
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var emsPatient = new ePatient()
+      {
+        ePatient01 = "42", // Identifier
+
+        ePatientPatientNameGroup = new ePatientEPatientPatientNameGroup
+        {
+          ePatient02 = new ePatientEPatientPatientNameGroupEPatient02
+          {
+            Value = "Twohey" // Last Name
+          },
+          ePatient03 = new ePatientEPatientPatientNameGroupEPatient03
+          {
+            Value = "Michael" // First Name
+          },
+          ePatient04 = "A" // Middle Initial
+        }
+      };
+
+      Console.WriteLine(
+        $"Patient Name { emsPatient.ePatientPatientNameGroup.ePatient03.Value } " +
+        $"{ emsPatient.ePatientPatientNameGroup.ePatient02.Value }");
+
+      Console.ReadLine();
+    }
+  }
+}
